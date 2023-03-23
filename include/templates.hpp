b0a7cf837d7814503960fc604ce3396c60174f4d
eightfold <97198162+8ightfold@users.noreply.github.com>
2023-03-22 22:23:56 -0600

    Update templates.hpp

--- include/templates.hpp
+++ include/templates.hpp
@@ -6,6 +6,7 @@
 #include <utility>
 #include "xtraits.hpp"
 
+/* $enum internals */
 namespace rust_enum {
     namespace meta {
         template <typename T, typename...TT>
@@ -136,6 +137,89 @@ namespace rust_enum {
         static constexpr auto get(const index_constant<N> i) noexcept {
             return type_internal(i);
         }
+    };
+}
+
+
+#include <map>
+#include <typeinfo>
+#include "meta/cttypeid.hpp"
+
+/* $vmatch internals */
+namespace rust_enum {
+    namespace meta {
+        constexpr std::uint64_t fnv1a_64(const std::string_view s, std::uint64_t count) {
+            constexpr std::uint64_t fnv_offset = 14695981039346656037llu;
+            constexpr std::uint64_t fnv_prime = 1099511628211llu;
+            return ((count ? fnv1a_64(s, count - 1) : fnv_offset) ^ s[count]) * fnv_prime;
+        }
+    }
+
+    template <typename T>
+    constexpr std::uint64_t type_hash() noexcept {
+        using U = std::remove_cvref_t<T>;
+        constexpr std::string_view name = cttypeid<U>.name();
+        return meta::fnv1a_64(name, name.size() - 1);
+    }
+
+    template <>
+    constexpr std::uint64_t type_hash<void>() noexcept { return 0; }
+
+    template <typename T>
+    inline constexpr std::uint64_t type_hash_v = type_hash<T>();
+
+
+    template <typename T, typename...UU>
+    [[nodiscard]] std::uint64_t vmatch_cases(const T* t) noexcept {
+        static const std::map<std::uint64_t, std::uint64_t> runtime_to_compile_time {
+            { typeid(UU).hash_code(), type_hash_v<UU> }...
+        };
+        std::uint64_t runtime_hash = typeid(*t).hash_code();
+
+        if(runtime_to_compile_time.contains(runtime_hash)) {
+            return runtime_to_compile_time.at(runtime_hash);
+        }
+        else return type_hash_v<void>;
+    }
+
+    template <typename T, typename...UU>
+    [[nodiscard]] std::uint64_t vmatch_cases(const T& t) noexcept {
+        return vmatch_cases<T, UU...>(&t);
+    }
+
+    template <typename T, typename U>
+    T& dispatch(U& u) noexcept { return dynamic_cast<T&>(u); }
+
+    template <typename T, typename U>
+    T* dispatch(U* u) noexcept { return dynamic_cast<T*>(u); }
+
+    template <typename T, typename U>
+    const T& dispatch(const U& u) noexcept { return dynamic_cast<const T&>(u); }
+
+    template <typename T, typename U>
+    const T* dispatch(const U* u) noexcept { return dynamic_cast<const T*>(u); }
+}
+
+
+/* generic internals */
+namespace rust_enum {
+    template <typename T>
+    concept is_rust_enum = std::is_base_of_v<enum_tag, std::remove_cvref_t<T>>;
+
+    template <typename T>
+    concept is_virtual = std::has_virtual_destructor_v<std::remove_cvref_t<std::remove_pointer_t<T>>>;
+
+    template <typename T>
+    struct invoke_or {
+        T expression;
+
+        template <bool B, typename...TT>
+        constexpr auto invoke(TT&&...tt) requires B {
+            return expression(std::forward<TT>(tt)...);
+        }
+
+        template <bool, typename...TT>
+        constexpr auto invoke(TT&&...) {}
     };
 }
 
